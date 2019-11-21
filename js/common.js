cbd21b3ff2ea83ffa0986e076a1f9df9bccf18d0
drone0898 <47773972+drone0898@users.noreply.github.com>
2019-11-22 00:23:46 +0900

    Add files via upload
    DVT

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(function(){
+    // ====================common=========================
+    location.href='#main';
+    
+    
+    
+    
+    
+
+    // =================#login====================
+    $(document).on('pagebeforeshow', '#login' ,function(){
+        location.href='#main';
+     });
+});
